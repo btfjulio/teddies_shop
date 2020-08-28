198f8253bc343f34f18e61979ea6251a84f32c6a
Bruno Tostes <btfjulio@hotmail.com>
2020-08-27 21:58:58 -0300

    add stripe payment test app

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+
+puts 'Cleaning database...'
+Teddy.destroy_all
+Category.destroy_all
+
+puts 'Creating categories...'
+geek = Category.create!(name: 'geek')
+kids = Category.create!(name: 'kids')
+
+puts 'Creating teddies...'
+Teddy.create!(sku: 'original-teddy-bear', name: 'Teddy bear', category: kids, price_cents: 790, photo_url: 'http://onehdwallpaper.com/wp-content/uploads/2015/07/Teddy-Bears-HD-Images.jpg')
+
+Teddy.create!(sku: 'jean-mimi', name: 'Jean-Michel - Le Wagon', category: geek, price_cents: 710, photo_url: 'https://pbs.twimg.com/media/B_AUcKeU4AE6ZcG.jpg:large')
+Teddy.create!(sku: 'octocat',   name: 'Octocat -  GitHub', category: geek, price_cents: 950, photo_url: 'https://cdn-ak.f.st-hatena.com/images/fotolife/s/suzumidokoro/20160413/20160413220730.jpg')
+puts 'Finished!'
